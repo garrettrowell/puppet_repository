ff86bad81921ec5bb18cf201b3e15fcf84a9ae2d
Garrett Rowell <Garrett2@cnwr.com>
2015-05-27 13:38:41 -0400

    ugh

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,6 +1,3 @@
-# Make filebucket 'main' the default backup location for all File resources:
-File { backup => 'main' }
-
 # Kill deprecation warnings in PE 3.3:
 Package { allow_virtual => false }
 
